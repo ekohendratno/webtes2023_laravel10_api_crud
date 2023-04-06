b598fc42e15ddcdaaef8d75183fd11d66c1af3ff
EKO HENDRATNO <eko.hendratno@gmail.com>
2023-04-06 11:16:48 +0700

    add api resource pegawai to routes
    testing api pegawai with postman for
    
    POST /api/pegawai
    GET /api/pegawai
    GET /api/pegawai/{pegawai_id}
    PUT /api/pegawai/{pegawai_id}
    DELETE /api/pegawai/{pegawai_id}

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::apiResource('/api/pegawai', App\Http\Controllers\Api\PegawaiController::class);

ae3a89ab36e4bcb9f7aa274632491e0977ea8f5c
EKO HENDRATNO <eko.hendratno@gmail.com>
2023-04-06 11:20:41 +0700

    add api resource jabatan to routes
    testing api jabatan with postman for
    
    POST /api/jabatan
    GET /api/jabatan
    GET /api/jabatan/{jabatan_id}
    PUT /api/jabatan/{jabatan_id}
    DELETE /api/jabatan/{jabatan_id}

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,3 +19,5 @@ Route::get('/', function () {
 
 
 Route::apiResource('/api/pegawai', App\Http\Controllers\Api\PegawaiController::class);
+Route::apiResource('/api/jabatan', App\Http\Controllers\Api\JabatanController::class);
+
